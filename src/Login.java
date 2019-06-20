3e63548c044f7f4a914919fe09bfe1de0df5e8b6
9731043 <9731043@ceit.local>
2019-06-20 15:14:52 +0430

    login Page and compare previous

---
+++ src/Login.java
@@ -0,0 +1,27 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class Login extends JFrame {
+    public Login() throws IOException, InterruptedException {
+        super("JPotify");
+        JLabel l=new JLabel();
+        Image img = ImageIO.read(getClass().getResource("music.png"));
+        Image img2=img.getScaledInstance(600,400,Image.SCALE_SMOOTH);
+        l.setIcon(new ImageIcon(img2));
+        this.setLayout(new BorderLayout());
+        this.add(l,BorderLayout.CENTER);
+        setSize(600, 400);
+        setMinimumSize(new Dimension(600, 400));
+        setLocationRelativeTo(null);
+        this.setVisible(true);
+        TimeUnit.SECONDS.sleep(3);
+        this.setVisible(false);
+    }
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Login l=new Login();
+        Login2 l2=new Login2();
+    }
+}
